d8714e6240f8b29cb4c68f4385a2c93854d77360
Jun Kurihara <lh182051+src@gmail.com>
2022-06-13 13:51:35 +0000

    :sparkles: add template

---
+++ template/src/features/counter/counterAPI.ts
@@ -0,0 +1,7 @@
+// A mock function to mimic making an async request for data
+/* eslint-disable-next-line @typescript-eslint/promise-function-async */
+export function fetchCount(amount = 1): Promise<{ data: number }> {
+  return new Promise<{ data: number }>((resolve) =>
+    setTimeout(() => resolve({ data: amount }), 500)
+  );
+}
